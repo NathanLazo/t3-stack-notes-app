f2bb40c0f1c4f7f4d7b158f57c7ee2de355a69a3
NathanLazo <nathanlazo@outlook.com>
2023-02-10 22:14:44 -0600

    [UPDATE]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,23 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
-  return (
+}) => (
+  <>
+    <Head>
+      <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+    </Head>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  );
-};
+  </>
+);
 
 export default api.withTRPC(MyApp);
