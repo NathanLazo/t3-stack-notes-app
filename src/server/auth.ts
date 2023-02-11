baa76b74d64608bc821b717298f1fb35cea58ab9
NathanLazo <nathanlazo@outlook.com>
2023-02-10 21:01:34 -0600

    [UPDATE] auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -64,6 +64,17 @@ export const authOptions: NextAuthOptions = {
      **/
   ],
   secret: env.NEXTAUTH_SECRET,
+  logger: {
+    error(code, ...message) {
+      console.error(code, message);
+    },
+    warn(code, ...message) {
+      console.warn(code, message);
+    },
+    debug(code, ...message) {
+      console.debug(code, message);
+    },
+  },
 };
 
 /**
